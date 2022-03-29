0f4592e1d6a9d90b38913e03ea0f605980b6f5e1
laura ducki <>
2022-03-29 17:58:27 +0200

    derniere modif de la journée

--- verfilettre.c
+++ verfilettre.c
@@ -7,11 +7,21 @@ verfilettre.c
 void verifplace (char mot , char notrechoix) {
     int i=0;
     int j=0;
-    
+    char* I=[];
+    char* J=[];
     for (i; i<5;i++) {
         for (j;j<5;j++){
-            if (mot[i]==notrechoix[j]) {printf "la lettre est dans le mot"}
+            if (mot[i]==notrechoix[j]) {
+                printf "la lettre est dans le mot";
+                if (i==j) {I=I+[mot(i)]}
+                else {J=J+[mot(i)]}
+                }
             
+        
         }
+    for (int i=0; i<strlen(I); i++) {printf "les lettres à la bonne place sont :" ;
+    printf("%d", I[i])}
+    for (int i=0 ; i<strlen(J) ; i++) {printf "Les lettres présentent mais à la mauvaise place sont : " ; printf ("%d", J[i]) 
     }
+        
 }
