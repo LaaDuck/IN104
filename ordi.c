351e7137355bcaf7ab3a3921baed84d1beef062f
laura ducki <>
2022-05-03 16:19:07 +0200

    seance 3 mai

---
+++ ordi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void ordi(char* tentative, char** dico, int ndico) {
+    // copier le dico : on va utiliser la copie dans ce dictionnaire
+    for (int i = 0; i < ndico; i++) {
+        for (int i=0 ; i<5 ; i++) {
+        if (tentative[i]==-1) {/*ajouter dans une liste tous les mots où la lettre verte est*/}
+
+            // ou avec la deuxième méthode : éliminer tous les mots du dico qui possèdent les lettres grises
+
+    }
+}
